d2603a99624948fd8330e882a5d5ed0bfb1baabc
Chris Buchanan <cbuchanan919@gmail.com>
2022-10-21 19:44:02 -0400

    Added getInput function

--- Proj1/main.cpp
+++ Proj1/main.cpp
@@ -1,19 +1,95 @@
 #include "Arduino.h"
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
-  This example code is in the public domain.
- */
+
+#define t_LED 2;
+#define EOL t_EOL 255;
+
+enum {
+	maxLen = 50
+};
+
+char nums[11] = "0123456789"; 
+char input[maxLen + 1]; // used to parse input from serial
+
+
+
+void clearInput();
+void getInput();
+
 
 void setup() {                
 	// initialize the digital pin as an output.
-	// Pin 13 has an LED connected on most Arduino boards:
-	pinMode(13, OUTPUT);     
+	Serial.begin(9600);
+	while (!Serial){ /* wait */ }
+	Serial.setTimeout(60000);
 }
 
 void loop() {
-	digitalWrite(13, HIGH);   // set the LED on
-	delay(1000);              // wait for a second
-	digitalWrite(13, LOW);    // set the LED off
-	delay(1000);              // wait for a second
-}   
+	clearInput();
+	getInput();	
+
+	//	Serial.println(input);
+}  
+
+void clearInput() {
+	for (int i = 0; i < maxLen + 1; i++){
+		input[i] = '\0';
+	}
+}
+
+
+/*
+ * This gets input from the console. It reads till the return key is pressed, or the max characters reached.
+ */
+void getInput(){
+	clearInput();
+	int index = 0;
+	int cont = 1;
+	int addChar = 1;
+	if (Serial){
+		Serial.println("Please enter command: ");
+		while (cont == 1) {
+			if (Serial.available() > 0){
+				addChar = 1;
+				char check = Serial.read();
+				if (check == 20 && input[index - 1] == 20) { /*No double spaces*/ addChar = 0; }
+				//				Serial.print(check, DEC);
+				//				Serial.print(' ');
+				if (index >= maxLen) { /* max input reached */ cont = 0; }
+				if (check == 13){ /* Return key entered */ cont = 0; }
+				if (check == 8 || check == 127) {
+					// backspace & delete
+					/*					
+					for (int i = 0; i < 1 && index >= 0; i++){
+						input[index] = '\0';
+						index--;
+					}
+					*/
+					index--;
+					input[index] = '\0';
+					
+					addChar = 0;
+					// print new line?
+					Serial.println();
+					Serial.print(input);
+
+				}
+
+				if (cont == 1 && addChar == 1) { 
+					input[index] = check; 
+					Serial.print(check);
+					index++;
+				}
+			}
+		}
+		input[index] = '\0';
+		Serial.println("\nYou Wrote: ");
+		Serial.println(input);
+	}
+}
+int getNumber(char *input, int size){
+	int result = 0;
+	for (int i = 0; i < size; i++){
+
+	}
+	return -1;	
+}
--- readme.md
+++ readme.md
@@ -1,4 +1,8 @@
 ## Welcome to Chris Buchanan's Digitize repo
+
+## Project 1 Overview:
+Project 1 will have you set up your project laptop and Arduino development environment, as well as begin on the core of your RTU - a command line interpreter. You will be required to take in a string from the keyboard, parse it, and execute different functions based on the contents. In addition to the laptop and Arduino board, you will also be given a bi-color LED to control.
+
 ### General Setup Info:
 My laptop is running Ubuntu 22.04.1 LTS and it has arduino version 1.8.19 installed. I'm using picocom to connect to arduino via serial, and using vim as the text editor. Somehow, Doxygen will be used to maintain a working documentation of the code. Git & github (https://github.com/cbuchanan919/Digitize) are being used to source control my code.
 
