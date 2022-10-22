2034f882526edbb337663f06bf05482399da7e95
Chris Buchanan <cbuchanan919@gmail.com>
2022-10-22 15:34:12 -0400

    Input finalized

--- Proj1/main.cpp
+++ Proj1/main.cpp
@@ -1,34 +1,34 @@
-#include "Arduino.h"
+#include <Arduino.h>
 
 #define t_LED 2;
 #define EOL t_EOL 255;
+
+// function headers
+void clearInput();
+void getInput();
+void printInputDetails();
 
 enum {
 	maxLen = 50
 };
 
+
 char nums[11] = "0123456789"; 
 char input[maxLen + 1]; // used to parse input from serial
 
-
-
-void clearInput();
-void getInput();
-
-
-void setup() {                
-	// initialize the digital pin as an output.
+void setup() {
 	Serial.begin(9600);
-	while (!Serial){ /* wait */ }
-//	Serial.setTimeout(100);
+	Serial.setTimeout(10000);
+	while (!Serial){ /*wait*/}
+	int cont = 1;
+
 }
 
-void loop() {
-	clearInput();
-	getInput();	
+void loop(){
+	getInput();
+	
+}
 
-	//	Serial.println(input);
-}  
 
 void clearInput() {
 	for (int i = 0; i < maxLen + 1; i++){
@@ -38,7 +38,8 @@ void clearInput() {
 
 
 /*
- * This gets input from the console. It reads till the return key is pressed, or the max characters reached.
+ * This gets input from the console. It reads till the return key is pressed, or the max characters reached. 
+ * It will write to the input variable (array).
  */
 void getInput(){
 	clearInput();
@@ -77,8 +78,27 @@ void getInput(){
 		Serial.println("\r\nYou Wrote: ");
 		Serial.println(input);
 		Serial.println();
+		//printInputDetails();
 	}
 }
+
+/*
+* This just prints out the details of each character in input. 
+* It iterates through each character of input and prints out the position followed by the value.
+*/
+void printInputDetails() {
+		Serial.println("Printing Input Details: ");
+		for (int i = 0; i < maxLen + 1; i++){
+			Serial.print(i);
+			Serial.print(" - ");
+			if (input[i] == '\0'){
+				Serial.println("null");
+			} else {
+				Serial.println(input[i]);
+			}
+		}
+}
+
 int getNumber(char *input, int size){
 	int result = 0;
 	for (int i = 0; i < size; i++){
