e9d54a56e22e6033dc6400cd9f7cb7a6d02373a6
Chris Buchanan <cbuchanan919@gmail.com>
2022-10-24 12:31:37 -0400

    Commands able to be interpreted & some implementation available

--- Proj1/main.cpp
+++ Proj1/main.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-//#include <Scheduler.h>
 
 // Define section: 
 #define t_OFF 0
@@ -40,7 +39,7 @@ int getInput();
 void printInputDetails();
 void parseInput();
 char getCommandFromWord(int start, int end);
-void applyCommands(char newCmd[4]);
+void applyCommands(char newCmd[6]);
 void ledStatus(LedSettings *led);
 void ledControl(LedSettings *led);
 void showHelp();
@@ -48,22 +47,6 @@ void showVersion();
 // ----------------
 
 
-/*
-   const char lookupTable[] = {
-   'o', 'f', 3, t_OFF, 
-   'o', 'n', 2, t_ON, 
-   'l', 'e', 3, t_LED,
-   'g', 'r', 5, t_GREEN,
-   'r', 'e', 3, t_RED,
-   's', 'e', 3, t_SET,
-   'b', 'l', 5, t_BLINK,
-   'd', '1', 3, t_D13,
-   's', 't', 6, t_STATUS,
-   'v', 'e', 7, t_VERSION,
-   'h', 'e', 4, t_HELP,
-   'd', 'e', 5, t_DEBUG
-   };
-   */
 
 const char lookupTable[] = {
 	'o', 'f', '3', t_OFF, 
@@ -91,7 +74,7 @@ int inputIndex = 0; 		// used to store current index of input.
 char commandTokens[maxLen + 1];
 int commandEntered = 1;
 
-char inDebugMode = t_DEBUG;	// if = t_DEBUG, shows more info in console.
+char inDebugMode = t_EOL;	// if = t_DEBUG, shows more info in console.
 LedSettings d13;
 LedSettings led;
 
@@ -154,7 +137,7 @@ int getInput(){
 		if (commandEntered == 1){
 			clearInput();
 			commandEntered = 0;
-			Serial.println("Please enter command: ");
+			Serial.println("\r\nPlease enter command: ");
 		}
 		if (Serial.available() > 0){
 			addChar = 1;
@@ -187,10 +170,13 @@ int getInput(){
 		if (commandEntered == 1){
 			// command entered. show what was entered.
 			input[inputIndex] = '\0';
-			Serial.println("\r\nYou Wrote: ");
-			Serial.println(input);
-			Serial.println();
-			printInputDetails();
+			if (inDebugMode == t_DEBUG){
+				Serial.println("\r\nYou Wrote: ");
+				Serial.println(input);
+				Serial.println();
+
+				printInputDetails();
+			}
 		}
 	}
 	return commandEntered;
@@ -223,10 +209,11 @@ void parseInput(){
 
 		}
 	}
-
-	Serial.print("\r\nI found ");
-	Serial.print(cmdIndex);
-	Serial.print(" commands.\r\n");
+	if (inDebugMode == t_DEBUG){
+		Serial.print("\r\nI found ");
+		Serial.print(cmdIndex);
+		Serial.print(" commands.\r\n");
+	}
 	if (cmdIndex > 0) {
 		commands[cmdIndex] = t_EOL;
 		applyCommands(commands);
@@ -245,157 +232,158 @@ char getCommandFromWord(int start, int end){
 			if (lookupTable[i] == input[start]){
 				if (lookupTable[i + 1] == input[start + 1]) {
 					int lookupLen = lookupTable[i + 2] - '0';
-					Serial.println("here2");
-					Serial.print(lookupLen);
-					Serial.print(" - ");
-					Serial.println(wordLen);
 					if (lookupLen == wordLen){
 						return lookupTable[i + 3]; 
 					}
 				}
 
-				}
-				//Serial.println(lookupTable[i]);
-			}	
+			}
+			//Serial.println(lookupTable[i]);
+		}	
+	}
+	if (inDebugMode == t_DEBUG){ Serial.println("no command found"); }
+	return t_NO_COMMAND_FOUND;
+}
+
+void applyCommands(char newCmd[6]){
+	int badDefault = 0;
+	switch (newCmd[0]) {
+		case t_DEBUG:
+			if (newCmd[1] == t_ON) {
+				inDebugMode = t_DEBUG;
+				Serial.println("debug mode on");
+			} else {
+				inDebugMode = t_EOL;
+				Serial.println("debug mode off");
+			}
+			break;
+		case t_HELP:
+			showHelp();
+			break;
+		case t_VERSION:
+			showVersion();
+			break;
+		default:
+			// do nothing
+			badDefault = 0;
+	}
+}
+
+/*
+ * This just prints out the details of each character in input. 
+ * It iterates through each character of input and prints out the position followed by the value.
+ */
+void printInputDetails() {
+	Serial.println("Printing Input Details: ");
+	for (int i = 0; i < maxLen + 1; i++){
+		Serial.print(i);
+		Serial.print(" - ");
+		if (input[i] == '\0'){
+			Serial.println("null");
+		} else {
+			Serial.println(input[i]);
 		}
-		Serial.println("no command found");
-		return t_NO_COMMAND_FOUND;
+	}
 }
 
 
-	void applyCommands(char newCmd[4]) {
-		int badDefault = 0;
-		switch (newCmd[4]) {
-			case t_DEBUG:
-				if (newCmd[1] == t_ON) {inDebugMode = t_DEBUG; }
-				else { inDebugMode = t_EOL; }
-				break;
-			default:
-				// do nothing
-				badDefault = 0;
-
-
-
-		}
+int getNumber(char *input, int size){
+	int result = 0;
+	for (int i = 0; i < size; i++){
 
 	}
+	return -1;	
+}
 
 
-	/*
-	 * This just prints out the details of each character in input. 
-	 * It iterates through each character of input and prints out the position followed by the value.
-	 */
-	void printInputDetails() {
-		Serial.println("Printing Input Details: ");
-		for (int i = 0; i < maxLen + 1; i++){
-			Serial.print(i);
-			Serial.print(" - ");
-			if (input[i] == '\0'){
-				Serial.println("null");
+void ledControl(LedSettings *led){
+	if (led->ledSetting == t_BLINK){
+		int currentMillis = millis();
+		if (currentMillis - led->previousMillis >= led->blinkRate){
+			led->previousMillis = currentMillis;
+			if (led->ledState == HIGH) {
+				led->ledState = LOW;
 			} else {
-				Serial.println(input[i]);
+				led->ledState = HIGH;
 			}
 		}
-	}
-
-
-	int getNumber(char *input, int size){
-		int result = 0;
-		for (int i = 0; i < size; i++){
-
-		}
-		return -1;	
-	}
-
-
-	void ledControl(LedSettings *led){
-		if (led->ledSetting == t_BLINK){
-			int currentMillis = millis();
-			if (currentMillis - led->previousMillis >= led->blinkRate){
-				led->previousMillis = currentMillis;
-				if (led->ledState == HIGH) {
-					led->ledState = LOW;
-				} else {
-					led->ledState = HIGH;
-				}
-			}
-
-
-		}
-		digitalWrite(led->ledPin, led->ledState);
-
-	}
-
-
-	/*
-	 * Prints to serial the status of the referenced led.
-	 *
-	 * */
-	void ledStatus(LedSettings *led){
-		Serial.print("\r\nPrinting led status for: ");
-		switch (led->ledName) {
-			case t_D13:
-				Serial.print("D13");
-				break;
-			case t_LED:
-				Serial.print("LED");
-				break;
-			default:
-				Serial.print("an led");
-		}
-		Serial.print(". \r\n");
-
-		if (led->ledSetting == t_BLINK){
-			Serial.print("It is set to blink every ");
-			Serial.print(led->blinkRate);
-			Serial.print(" ms.\r\n");
-		} else {
-			Serial.print("It is turned ");
-			if (led->ledSetting == t_ON) { 
-				Serial.print(" on.\r\n"); }
-			else {
-				Serial.print(" off.\r\n"); }
-		}
-		Serial.print("The color is ");
-		switch (led->ledPin){
-			case t_GREEN:
-				Serial.print("green");
-				break;
-			case t_RED:
-				Serial.print("red");
-				break;
-			default:
-				Serial.print("white");
-		}
-		Serial.print(".\r\n");
-
-	}
-
-
-	/*
-	 * This prints a help menu of available commands.
-	 *
-	 * */
-	void showHelp(){
-		Serial.print("Showing help for Arduino Project 1, Version ");
-		Serial.print(appVersion);
-		Serial.print(". \r\n");
-		Serial.println("Here are some of the commands that can be entered: ");
-
-		Serial.println("debug on - Turns on debug mode. This shows more debug info");
-		Serial.println("debug off - Turns off debug mode.\r\n");
-
-
 
 
 	}
+	digitalWrite(led->ledPin, led->ledState);
+
+}
 
 
-	void showVersion(){
-		Serial.print("\r\nThe current version of this app is: ");
-		Serial.print(appVersion);
-		Serial.print("\r\n");
+/*
+ * Prints to serial the status of the referenced led.
+ *
+ * */
+void ledStatus(LedSettings *led){
+	Serial.print("\r\nPrinting led status for: ");
+	switch (led->ledName) {
+		case t_D13:
+			Serial.print("D13");
+			break;
+		case t_LED:
+			Serial.print("LED");
+			break;
+		default:
+			Serial.print("an led");
 	}
+	Serial.print(". \r\n");
+
+	if (led->ledSetting == t_BLINK){
+		Serial.print("It is set to blink every ");
+		Serial.print(led->blinkRate);
+		Serial.print(" ms.\r\n");
+	} else {
+		Serial.print("It is turned ");
+		if (led->ledSetting == t_ON) { 
+			Serial.print(" on.\r\n"); }
+		else {
+			Serial.print(" off.\r\n"); }
+	}
+	Serial.print("The color is ");
+	switch (led->ledPin){
+		case t_GREEN:
+			Serial.print("green");
+			break;
+		case t_RED:
+			Serial.print("red");
+			break;
+		default:
+			Serial.print("white");
+	}
+	Serial.print(".\r\n");
+
+}
+
+
+/*
+ * This prints a help menu of available commands.
+ *
+ * */
+void showHelp(){
+	Serial.print("\r\nShowing help for Arduino Project 1, Version ");
+	Serial.print(appVersion);
+	Serial.print(". \r\n");
+	Serial.println("Here are some of the commands that can be entered: ");
+
+	Serial.println("\tdebug on - Turns on debug mode. This shows more debug info");
+	Serial.println("\tdebug off - Turns off debug mode.\r\n");
+
+
+
+
+}
+
+
+void showVersion(){
+	Serial.print("\r\nThe current version of this app is: ");
+	Serial.print(appVersion);
+	Serial.print("\r\n");
+}
 
 
 
